326c457acd3524d4a6f91cc6db44c1eddb0a70c3
ccravens <chad.cravens@ossys.com>
2014-06-24 13:07:35 -0400

    Added variables, array and function to hello program

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+void print_fav_numbers(int fav_numbers[]) {
+	std::cout << "My favorite numbers are:" << std::endl;
+	for(int i=0; i<14; i++) {
+		std::cout << "Index " << i << ": " << fav_numbers[i] << std::endl;
+	}
+}
+
 int main(void) {
-	std::cout << "Hello!" << std::endl;
+	int age;
+	age = 31;
+	
+	int numbers[14] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43};
+
+	std::cout << "Hello! Chad is " << age << std::endl;
+
+	print_fav_numbers(numbers);
+
 	return 0;
 }
